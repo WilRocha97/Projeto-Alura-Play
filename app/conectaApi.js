3e65fdeb7458474555739cb613f6530fc4ca6f22
Willian Rodrigues <willrbr1997@gmail.com>
2023-06-27 16:32:25 -0300

    implementando busca de video
    Implementando busca de vídeo e página para adicionar vídeos (O código para adicionar existe mas foi comentado para evitar envio de vídeos indesejáveis)

--- app/conectaApi.js
+++ app/conectaApi.js
@@ -4,6 +4,33 @@ async function listaVideos() {
     return conexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+// exporta o resultado para outros arquivos poderem usar
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo,
+    buscaVideo
 }
